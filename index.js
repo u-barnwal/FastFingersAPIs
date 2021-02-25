d4a68b2c073a53d9515acde611cbcf5ef2fc8a54
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-26 01:04:05 +0530

    validating request params before register and sending error codes

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const db = mysql.createPool({
   database: "fast_fingers",
 });
 
+const CODE = {
+  INTERNAL_SERVER_ERROR: 500,
+  SUCCESS: 200,
+  BAD_REQUEST: 400,
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,16 +25,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (request, response) => response.send("404: Nothing here!"));
 
 // - players
-app.post("/apis/players/insert", (request, response) => {
+// app.post("/apis/players/login", (request, response) => {
+//   const { email, password } = request.body;
+
+//   db.query(
+//     "SELECT * FROM players WHERE email=? AND password=?",
+//     [email, password],
+//     (error, result) => {
+//       console.log(result);
+//       response.send(result);
+//     }
+//   );
+// });
+
+app.post("/apis/players/register", (request, response) => {
   const { name, email, password } = request.body;
+
+  if (!validateMandatoryParams([name, email, password]))
+    response.sendStatus(CODE.BAD_REQUEST);
 
   db.query(
     "INSERT INTO players (name, email, password) VALUES (?, ?, ?)",
     [name, email, password],
     (error, result) => {
-      console.log(result);
+      if (error) response.status(CODE.INTERNAL_SERVER_ERROR).send();
+      else response.status(CODE.SUCCESS).send(result);
     }
   );
 });
 
-app.listen(3000, () => console.log("Server started on Port 3000"));
+app.listen(3000);
+
+// - Utils functions
+
+function validateMandatoryParams(params = []) {
+  for (const param of params) {
+    if (!param || param === "") return false;
+  }
+
+  return true;
+}
+
+function validateOptionalParams(params = []) {
+  return true;
+}

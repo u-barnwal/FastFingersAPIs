c790b0048c571f2e7eb8477b9646fec8e5f66a38
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-25 21:30:58 +0530

    install cors, several config changes and create players/insert api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +12,23 @@ const db = mysql.createPool({
   database: "fast_fingers",
 });
 
-app.get("/", (req, res) => {});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => response.send("404: Nothing here!"));
+
+// - players
+app.post("/apis/players/insert", (request, response) => {
+  const { name, email, password } = request.body;
+
+  db.query(
+    "INSERT INTO players (name, email, password) VALUES (?, ?, ?)",
+    [name, email, password],
+    (error, result) => {
+      console.log(result);
+    }
+  );
+});
 
 app.listen(3000, () => console.log("Server started on Port 3000"));

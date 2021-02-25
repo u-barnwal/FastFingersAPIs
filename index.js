0f07d263eb7c730a9946ed6c99dd3eabed55502c
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-25 19:39:31 +0530

    add db

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const mysql = require("mysql");
 
 const app = express();
 
-app.listen("3000", () => console.log("Server started on Port 3000"));
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "fast_fingers",
+});
+
+app.get("/", (req, res) => {});
+
+app.listen(3000, () => console.log("Server started on Port 3000"));

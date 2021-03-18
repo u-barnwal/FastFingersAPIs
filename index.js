5e1faa4a724d902f06900de4041bad82e9ea8365
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-19 04:29:10 +0530

    minor change

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
 });
 
 const jwt = require("jsonwebtoken");

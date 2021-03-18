f2e675a9461c7265f9122fc1358ec5a78a371b84
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-19 04:11:50 +0530

    passing port with db

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
 });
 
 const jwt = require("jsonwebtoken");

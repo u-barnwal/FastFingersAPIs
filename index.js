52042d53cc394d336defd956a6b8b30c45489b02
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-19 01:17:48 +0530

    commented out db

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const cors = require("cors");
 
 const app = express();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
 const jwt = require("jsonwebtoken");
 
